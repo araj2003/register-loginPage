37076fba4d2c4d10a57b882420f49b6ef6713f1d
araj <araj.jain2003@gmail.com>
2023-12-22 00:06:43 +0530

    created logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import axios from 'axios'
 const Login = () => {
+
+  const [data,setData] = useState({
+    email:"",
+    password:""
+  })
+
+  const registerUser = (e) => {
+    e.preventDefault()
+    console.log(data)
+    axios.get('/')
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <h1>Login</h1>
+      <div>
+      <form onSubmit={registerUser}>
+          <label>Email</label>
+          <input type="email" placeholder='Enter email' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+          <label>Password</label>
+          <input type="password" placeholder='Enter password' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
   )
 }
 
---
+++ server/App.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = require('./routes/authRoute')
+const connectDB = require('./connect')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const app = express()
+
+
+//middleware
+app.use(bodyParser.json()); 
+app.use('/',router)
+
+// app.get('/',(req,res) => {
+//     res.json('hello world')
+// })
+
+const port = 3000
+const start = async() => {
+    try {
+
+        await connectDB(process.env.MONGO_URI)
+        console.log('connected')
+        app.listen(port,() => {
+          console.log(`server is running on port ${port}`)  
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
